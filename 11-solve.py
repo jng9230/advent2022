2bdba53040ef933a3844235c0ee0466ea29dfea8
jng9230 <jng9230@gmail.com>
2022-12-26 22:07:10 -0500

    day11 refactor

--- 11-solve.py
+++ 11-solve.py
@@ -1,11 +1,20 @@
 from collections import deque
-import heapq
 from math import gcd # Python versions 3.5 and above
 from functools import reduce # Python version 3.x
 
 def to_dict(text):
     """
-    Converts the input to a dict 
+    Converts the input to a dict with fields as such:
+    key: int/monkey number:
+    value: {
+        "items": [int], - items that the monkey has
+        "test": int, - number to be modulo'd
+        "test1": int, - monkey to throw to if modulo works
+        "test0": int, - monkey to throw to if module doesn't work
+        "inspections": 0, - # of items monkey has seen
+        "op": + or *, - operator for monkey's operation 
+        "operand": int or "old"
+    }
     """
     temp = {}
     for i in range(len(text)):
@@ -21,48 +30,19 @@ def to_dict(text):
         #determine the operation done by the monkey
         op_splits = lines[2].split(" ")
         op = op_splits[-2]
-        rhs = op_splits[-1]
-        if op == "+":
-            func = lambda x : x + (int(rhs) if rhs != "old" else x)
-        else:
-            func = lambda x : x * (int(rhs) if rhs != "old" else x)
+        operand = op_splits[-1]
 
-        # print(f"name: {name}")
-        # print(func(2))
-        # print(f"items: {items}")
-        # print(f"test: {test}")
-        # print(f"test1: {test1}")
-        # print(f"test0: {test0}\n\n")
         temp[name] = {
             "items": items, 
             "test": test,
             "test1": test1,
             "test0": test0,
             "inspections": 0,
-            "test_op": func(2),
-            "op": func
+            "op": op,
+            "operand": operand
         }
     
     return temp
-
-FUCK1 = [
-    lambda x : x * 19,
-    lambda x : x + 6, 
-    lambda x : x * x,
-    lambda x : x + 3
-]
-
-FUCK = [
-    lambda x : x * 3,
-    lambda x : x + 8,
-    lambda x : x * x,
-    lambda x : x + 2,
-    lambda x : x + 3,
-    lambda x : x * 17, 
-    lambda x : x + 6,
-    lambda x : x + 1
-]
-
 
 def solve():
     """
@@ -83,7 +63,12 @@ def solve():
                 m["inspections"] += 1
                 item = m["items"].popleft()#inspect item 
                 # item1 = m["op"](item) #inspect, change worry
-                item1 = FUCK[i](item)
+                # item1 = FUCK[i](item)
+                rhs = item if m["operand"] == "old" else int(m["operand"])
+                if m["op"] == "+":
+                    item1 = item + rhs 
+                else:
+                    item1 = item * rhs
                 item2 = item1 // 3 #drop worry b/c no damage
                 if item2 % m["test"] == 0: #test and toss
                     monkeys[m["test1"]]["items"].append(item2)
@@ -91,12 +76,8 @@ def solve():
                     monkeys[m["test0"]]["items"].append(item2)
 
     #monkey business: the multiple of two largest inspections
-    res_arr = []
-    for i in range(num_monkeys):
-        res_arr.append(monkeys[i]["inspections"])
-    heapq.heapify(res_arr)
-    while len(res_arr) != 2:
-        heapq.heappop(res_arr) 
+    res_arr = [v["inspections"] for k,v in monkeys.items()]
+    res_arr.sort(reverse=True)
 
     return res_arr[0] * res_arr[1]
 
@@ -118,32 +99,30 @@ def solve2():
         """
         return reduce(lambda a,b: a*b // gcd(a,b), denominators)
 
-    thing = []
-    for i in range(num_monkeys):
-        thing.append(monkeys[i]["test"])
-    LCM = lcm(thing)
+    LCM = lcm([v["test"] for _, v in monkeys.items()])
 
     #simulate the monkeys throwing 
-    for r in range(num_rounds):
+    for _ in range(num_rounds):
         for i in range(num_monkeys):
             m = monkeys[i]
             while len(m["items"]): #check only if there are items
                 m["inspections"] += 1
                 item = m["items"].popleft()#inspect item 
                 # item1 = m["op"](item) #inspect, change worry
-                item1 = FUCK[i](item)
+                rhs = item if m["operand"] == "old" else int(m["operand"])
+                if m["op"] == "+":
+                    item1 = item + rhs 
+                else:
+                    item1 = item * rhs
                 item2 = item1 % LCM
                 if item2 % m["test"] == 0: #test and toss
                     monkeys[m["test1"]]["items"].append(item2)
                 else: 
                     monkeys[m["test0"]]["items"].append(item2)
 
-    res_arr = []
-    for i in range(num_monkeys):
-        res_arr.append(monkeys[i]["inspections"])
-    heapq.heapify(res_arr)
-    while len(res_arr) != 2:
-        heapq.heappop(res_arr) 
+    #monkey business: the multiple of two largest inspections
+    res_arr = [v["inspections"] for _,v in monkeys.items()]
+    res_arr.sort(reverse=True)
 
     return res_arr[0] * res_arr[1]
 
